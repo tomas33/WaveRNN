ea63895810241e348881823cb958e80cfe245981
Tomas lloret <tanquealc@hotmail.com>
2021-02-06 15:09:57 +0100

    Update fatchord_version.py

--- models/fatchord_version.py
+++ models/fatchord_version.py
@@ -257,7 +257,7 @@ class WaveRNN(nn.Module):
         output = output[:wave_len]
         output[-20 * self.hop_length:] *= fade_out
 
-        save_wav(output, save_path)
+        soundfile.write(output, save_path)
 
         self.train()
 
